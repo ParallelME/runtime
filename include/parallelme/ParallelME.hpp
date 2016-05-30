3b48df61643a0f3ed5b503c99ab040085446bda9
Renato Utsch <renatoutsch@gmail.com>
2016-05-30 14:42:56 -0300

    Add new includes to the ParallelME.hpp file.

--- include/parallelme/ParallelME.hpp
+++ include/parallelme/ParallelME.hpp
@@ -9,10 +9,19 @@
 #ifndef PARALLELME_PARALLELME_HPP
 #define PARALLELME_PARALLELME_HPP
 
+/*
+ * This file includes all the other ParallelME header files for just one
+ * include file.
+ */
+
 #include "Buffer.hpp"
+#include "Device.hpp"
+#include "Kernel.hpp"
 #include "Program.hpp"
 #include "Runtime.hpp"
+#include "SchedulerFCFS.hpp"
+#include "SchedulerHEFT.hpp"
+#include "SchedulerPAMS.hpp"
 #include "Task.hpp"
-#include "Kernel.hpp"
 
 #endif // !PARALLELME_PARALLELME_HPP
